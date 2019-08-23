5d0985564c5de0a85194720a3ecf002e2f7a6763
loicbtd <loicbtd@users.noreply.github.com>
2019-08-23 19:05:52 -0400

    tout fonctionne

--- src/donnee/VoitureSQL.java
+++ src/donnee/VoitureSQL.java
@@ -2,8 +2,8 @@ package donnee;
 
 public interface VoitureSQL {
 	
-	public static final String SQL_LISTER_VOITURES = "SELECT * FROM garage";
-	public static final String SQL_AJOUTER_VOITURES = "INSERT into garage(modele, couleur, puissance, annee) VALUES(?,?,?,?)";
-	public static final String SQL_MODIFIER_VOITURES = "UPDATE garage SET modele = ?, couleur = ?, puissance = ?, annee = ? WHERE id = ?";
-	public static final String SQL_RAPPORTER_VOITURES = "SELECT * FROM garage WHERE id = ?";
+	public static final String SQL_LISTER_VOITURES = "SELECT * FROM voiture";
+	public static final String SQL_AJOUTER_VOITURES = "INSERT into voiture(modele, couleur, puissance, annee) VALUES(?,?,?,?)";
+	public static final String SQL_MODIFIER_VOITURES = "UPDATE voiture SET modele = ?, couleur = ?, puissance = ?, annee = ? WHERE id = ?";
+	public static final String SQL_RAPPORTER_VOITURES = "SELECT * FROM voiture WHERE id = ?";
 }
